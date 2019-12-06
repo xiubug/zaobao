d36995e9d898d88cc40ee65bbc27d13cd8216f28
魏昌华 <weichanghua@linctex.com>
2019-12-06 18:52:50 +0800

    feat: init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,98 @@ module.exports = {
     nav:[ // 导航栏配置
       {text: 'GitHub', link: 'https://github.com/sosout', target: '_self', rel: ''}
     ],
-    sidebar: 'auto', // 侧边栏配置
+    sidebar: [
+      ["/", "1. 介绍"],
+      {
+        title: "2019年12月",
+        collapsable: false,
+        children: readFileList("2019", "12"),
+      },
+      {
+        title: "2019年11月",
+        collapsable: false,
+        children: readFileList("2019", "11"),
+      },
+      {
+        title: "2019年10月",
+        collapsable: false,
+        children: readFileList("2019", "10"),
+      },
+      {
+        title: "2019年09月",
+        collapsable: false,
+        children: readFileList("2019", "09"),
+      },
+      {
+        title: "2019年08月",
+        collapsable: true,
+        children: readFileList("2019", "08"),
+      },
+      {
+        title: "2019年07月",
+        collapsable: true,
+        children: readFileList("2019", "07"),
+      },
+      {
+        title: "2019年06月",
+        collapsable: true,
+        children: readFileList("2019", "06"),
+      },
+      {
+        title: "2019年05月",
+        collapsable: true,
+        children: readFileList("2019", "05"),
+      },
+      {
+        title: "2019年04月",
+        collapsable: true,
+        children: readFileList("2019", "04"),
+      },
+      {
+        title: "2019年03月",
+        collapsable: true,
+        children: readFileList("2019", "03"),
+      },
+      {
+        title: "2019年02月",
+        collapsable: true,
+        children: readFileList("2019", "02"),
+      },
+      {
+        title: "2019年01月",
+        collapsable: true,
+        children: readFileList("2019", "01"),
+      },
+      {
+        title: "2018年12月",
+        collapsable: true,
+        children: readFileList("2018", "12"),
+      },
+      {
+        title: "2018年11月",
+        collapsable: true,
+        children: readFileList("2018", "11"),
+      },
+      {
+        title: "2018年10月",
+        collapsable: true,
+        children: readFileList("2018", "10"),
+      },
+      {
+        title: "2018年09月",
+        collapsable: true,
+        children: readFileList("2018", "09"),
+      },
+      {
+        title: "2018年08月",
+        collapsable: true,
+        children: readFileList("2018", "08"),
+      },
+    ],
+    algolia: {
+      apiKey: "45ccc89b4b1e0e9463c80a66f168ae6d",
+      indexName: "sosout_tqt",
+    },
   },
   configureWebpack: {
     resolve: {
